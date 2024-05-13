8fd501a3fc4e05867f073c18de154dbd18fbf931
Krit <jasada.k04@gmail.com>
2024-05-13 14:25:44 +0700

    Feat : choose func do you want

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ def welcomeMes():
     print("เบอร์โทร/เลขต่างๆ กด 2")
 
 
+def chooseFunction():
+    try:
+        x = int(input("ฉันเลือก : "))
+        while (x != 1) or (x != 2):
+            print("กรุณาเลือกให้ถูกต้อง!")
+            chooseFunction()
+    except:
+        print("กรุณาเลือกให้ถูกต้อง!")
+        chooseFunction()
+    
 
